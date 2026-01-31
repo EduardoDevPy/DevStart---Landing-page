8d245c833c5babd0536093bec47d626d38546ac1
Eduardo Vieira <evieira142@gmail.com>
2026-01-30 22:39:35 -0300

    feat: add smooth scroll navigation with JavaScript

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Seleciona todos os links do menu
+const menuLinks = document.querySelectorAll('.header__menu a')
+
+//Percorre cada link do menu
+menuLinks.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault()
+        // Pega o id da seção
+        const targetId = link.getAttribute('href')
+        // Seleciona a seção correspondente
+        const targetSection = document.querySelector(targetId)
+        // Faz o scroll suave até a seção
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        })
+    })
+})
